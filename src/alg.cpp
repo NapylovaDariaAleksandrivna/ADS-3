7c787b7280727b16f431f4193f8fe9bd41e75abc
NapylovaDariaAleksandrivna <124875138+NapylovaDariaAleksandrivna@users.noreply.github.com>
2023-03-12 16:14:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,39 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+
+int cbinsearch(int* arr, int size, int value) {
+  int count = 0, l = 0, r = size / 2;
+  while (r - l > 0) {
+    if (arr[r - l / 2] > value) {
+      r = r - l / 2 - 1;
+    }
+    else if (arr[r - l / 2] < value) {
+      l = r - l / 2 + 1;
+    }
+    else {
+      count += 1;
+      int cur = r - l / 2 - 1;
+      while (cur > 0) {
+        if (arr[cur] == value) {
+          count += 1;
+        }
+        else {
+          break;
+        }
+        cur -= 1;
+      }
+      cur = r - l / 2 + 1;
+      while (cur > 0) {
+        if (arr[cur] == value) {
+          count += 1;
+        }
+        else {
+          break;
+        }
+        cur += 1;
+      }
+      break;
+    }
+  }
+  return count;
 }
